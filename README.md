6f7fd72cf555bf35ad6299e73390e598f035b13f
Doron Segal <doron2402@gmail.com>
2015-04-03 08:50:40 -0700

    test and readme are ready

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # moonstatus
+
+## Why
+  We want to add a route to Express.js or Hapi.js that will give us the instance status memory, cpu and disk usage.
+
+## How
+  Using the native node.js 'os'
+
+
+## Getting Started
+
+  var ms = require('moonstatus');
+  ms.init({drive: '/'}, function(err, result) {
+    //result will contain all the values you need.
+    {
+      Memory: {
+        totalMemory: Number,
+        useMemory: Number,
+        precentageMemory: String
+      },
+      Cpu: {
+        Cpus: [ Object,.. ],
+        Counter: Number
+      },
+      Cpu_Architecture: String,
+      Network: {
+        lo0: [ [Object], [Object], [Object] ],
+        en0: [ [Object], [Object] ],
+        vboxnet0: [ [Object] ]
+      },
+      OS: {
+        Version: String,
+        Type: String,
+        Platform: String
+      },
+      Disk: {
+        total: Number,
+        free: Number,
+        status: String,
+        percentageDisk: String
+      }
+    }
+  });
+
+
+### When Using Express
+  var ms = require('moonstatus');
+  app.get('/instance/status', function(request, response){
+    ms.init({drive: '/'}, function(err, result) {
+      if (err) {
+        Logger(err); //You probably will want to log it.
+      }
+      return response.send(result);
+    });
+  });
+
+### When Using Hapi.js
+  var ms = require('moonstatus');
+  server.route({
+    method: 'GET',
+    path: '/instance/status',
+    handler: function (request, reply) {
+      ms.init({drive: '/'}, function(err, result) {
+        if (err) {
+          Logger(err); //You probably will want to log it.
+        }
+        return reply(result);
+      });
+    }
+  });
+
+
+## Good Luck!
--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,99 @@
 'use strict';
-var assert = require("assert")
-var moonStatus = require("../lib");
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-});
+var assert = require('assert');
+var should = require('should');
+var moonStatus = require('../lib');
 
 describe('MoonStatus', function(){
-  describe('When passing empty option object', function(){
+
+  describe('When passing empty options object', function(){
     var options = {};
-    it('should return object', function(done){
-      moonStatus.init(options, function(err, res){
-        console.log(err);
-        console.log(res);
+    it('should return object with all the right properties', function(done){
+      moonStatus.init(options, function(err, result){
+        should.not.exist(err);
+        should.exist(result);
+        result.should.have.property('Memory');
+        result.should.have.property('Cpu');
+        result.should.have.property('Cpu_Architecture');
+        result.should.have.property('Network');
+        result.should.have.property('OS');
+        result.should.have.property('Disk');
         done();
       });
     });
-  })
+  });
+
+  describe('When passing a null option', function(){
+    it('should return object with all the right properties', function(done){
+      moonStatus.init(null, function(err, result){
+        should.not.exist(err);
+        should.exist(result);
+        result.should.have.property('Memory');
+        result.should.have.property('Cpu');
+        result.should.have.propertyByPath('Cpu', 'Counter').above(0);
+        result.should.have.property('Cpu_Architecture');
+        result.should.have.property('Network');
+        result.should.have.property('OS');
+        result.should.have.property('Disk');
+        done();
+      });
+    });
+  });
+
+  describe('When options.drive is unknown', function() {
+    var options = {
+      drive: '/unknownDrive'
+    };
+    it('should return object with all the right properties', function(done){
+      moonStatus.init(options, function(err, result){
+        err.should.have.property('killed',false);
+        err.should.have.property('code',1);
+        err.should.have.property('signal',null);
+        should.exist(result);
+        result.should.have.property('Memory');
+        result.should.have.property('Cpu');
+        result.should.have.property('Cpu_Architecture');
+        result.should.have.property('Network');
+        result.should.have.property('OS');
+        result.should.have.property('Disk');
+        result.should.have.propertyByPath('Disk', 'stderr');
+        result.should.have.propertyByPath('Disk', 'stdout');
+        done();
+      });
+    });
+  });
+
+  describe('When options.drive is set to mount drive `/` -- (Running On Mac/Linux) --', function() {
+    var options = {
+      drive: '/'
+    };
+    it('should return object with all the right properties', function(done){
+      moonStatus.init(options, function(err, result){
+        should.not.exist(err);
+        should.exist(result);
+
+        result.should.have.propertyByPath('Memory','totalMemory');
+        result.should.have.propertyByPath('Memory','useMemory');
+        result.should.have.propertyByPath('Memory','precentageMemory');
+
+        result.should.have.propertyByPath('Cpu','Cpus');
+        result.should.have.propertyByPath('Cpu','Counter');
+
+        result.should.have.property('Cpu_Architecture');
+
+        result.should.have.property('Network');
+
+        result.should.have.propertyByPath('OS','Version');
+        result.should.have.propertyByPath('OS','Type');
+        result.should.have.propertyByPath('OS','Platform');
+
+        result.should.have.propertyByPath('Disk', 'total');
+        result.should.have.propertyByPath('Disk', 'free');
+        result.should.have.propertyByPath('Disk', 'status');
+        result.should.have.propertyByPath('Disk', 'percentageDisk');
+        done();
+      });
+    });
+  });
+
 });
 
